b2b74b038e0fed94f819f196849482ec8e6070e5
KaiZuke <38561387+KaiZuke@users.noreply.github.com>
2018-04-21 15:09:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ bot.on('message', (message) => {
     }
     
     if(message.content == 'spam!!!') {
-        message.reply('THE SPAMMER HAS DERANKED BECAUSE OF SPAMMING!! DONT SPAM GAMERS!!');
+        message.channel.sendMessage('THE SPAMMER HAS DERANKED BECAUSE OF SPAMMING!! DONT SPAM GAMERS!!');
     }
 
 });
