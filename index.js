b95fea6630887ed5db0efd8d24b1a55d5422d848
KaiZuke <38561387+KaiZuke@users.noreply.github.com>
2018-05-13 14:53:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const bot = new Discord.Client();
 
 bot.on('ready', () => { bot.user.setGame('say !help') });
 bot.on('message', (message) => {
-
-    if(message.content == 'say') {
-        const sayMessage = args.join(" ");
-        message.delete().catch(O_o=>{});
-        message.channel.send(sayMessage);
-    }
     
     if(message.content == '!help') {
         message.channel.sendMessage('Hi There! Saya adalah AI yang dibuat oleh KaiZuke atau Sean Edbert \n Tugas saya adalah membantu kamu yang memerlukan bantuan! \n COMMANDS : \n !ranks untuk melihat rank apa saja di server ini \n !credits untuk melihat credits \n ?derank untuk mengetahui lebih lanjut cara derank \n !kick untuk mengetahui aturan KICK dan BAN ');
